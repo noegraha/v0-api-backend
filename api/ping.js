4d6752a6b728bd1789a3b096e06c4b64892b8cc5
Nugraha <nugraha.triatmaja@gmail.com>
2025-12-18 06:36:16 +0700

    delete health and ping

--- api/ping.js
+++
@@ -1,8 +0,0 @@
-// api/ping.js
-export const runtime = 'nodejs';
-export default function handler(req, res) {
-  res.status(200).json({
-    message: "pong",
-    timestamp: new Date().toISOString(),
-  })
-}
